37b599f8e3f61efb2c602b895ecd42fdb4ced7c4
havenmini <haven.mini3@gmail.com>
2024-09-21 09:08:33 +0545

    my first commit

---
+++ Class/02_Package.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+array = np.sqrt([1, 2, 3])
+print(array)
---
+++ Class/03_Nepali Date and time.py
@@ -0,0 +1,9 @@
+import nepali_datetime as ndt
+
+# Get the current Nepali date and time
+nepali_date = ndt.date.today()
+nepali_time = ndt.datetime.now()
+
+# Print the Nepali date and time
+print("Nepali Date:", nepali_date)
+print("Nepali Time:", nepali_time)
