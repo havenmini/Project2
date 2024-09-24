689c367e34a5dd0d4d8b4666b57392982d509ab6
havenmini <haven.mini3@gmail.com>
2024-09-24 12:47:55 +0545

    all file added

--- Exercise/05_Exercise.py
+++ Exercise/05_Exercise.py
@@ -1,12 +1,12 @@
 class Person:
-    def __init__(self, name, age, city):
-        self.name = name
-        self.age = age
-        self.city = city
+    def __init__(self):
+        self.name = input("Enter your name: ")
+        self.age = int(input("Enter your age: "))
+        self.city = input("Enter your city: ")
 
     def introduce(self):
         print(f"Hello, my name is {self.name}. I am {self.age} years old and I live in {self.city}.")
 
 # उदाहरणको लागि
-person1 = Person("Ram", 25, "Kathmandu")
+person1 = Person()
 person1.introduce()
